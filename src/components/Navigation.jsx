2e4ee39c55b68677ba1ba3c75465cddbe43e73f6
halimaHussein <hanahussein6155@gmail.com>
2022-12-26 19:19:54 +0300

    commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,20 @@ import React from "react";
 const Navigationcomponent = ()=>{
     return(
         < nav className="navbar navbar-expand-lg nav-dark bg-dark" >
-        <a className="navbar-brand" href="/"> React firebase file managment system</a>
+        
+        <a className="navbar-brand" href="/"> React firebase file managment system
+        </a>
+
+        <ul className="navbar-nav ms-auto me-5">
+            <li className=" btn btn-success btn-sms">
+                Login
+            </li>
+            <li className="nav-item" mx-2>
+                <a className=" btn btn-primary btn-sm" href="/login">
+                    Register
+                </a>
+            </li>
+        </ul>
         </nav>
     )
 }
